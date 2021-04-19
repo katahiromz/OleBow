317d89c317c4c87ba5016f266507742cff931506
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-04-19 09:36:18 +0900

    restructuring

---
+++ README.md
@@ -0,0 +1,5 @@
+# OleBow by katahiromz
+
+The OleView replacement in plain C++11/Win32.
+
+This software is based on [leibnitz27/olewoo](https://github.com/leibnitz27/olewoo).
--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,5 @@ make -j3 || exit;
 for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
 do
 	echo Testing for ${i}.tlb...
-	./olebow32.exe ../x86-testdata/${i}.tlb ../x86-testdata/${i}-3.txt
+	./src/olebow32.exe ../x86-testdata/${i}.tlb ../x86-testdata/${i}-3.txt
 done

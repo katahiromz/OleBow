9f33037bbc8130c5d58b398e2e2f4a0821de8830
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-04-19 18:38:24 +0900

    add test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
+do
+	echo Testing for ${i}.tlb...
+	./src/olebow.exe ../x86-testdata/${i}.tlb ../x86-testdata/${i}-3.txt
+done
